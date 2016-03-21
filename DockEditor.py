1e1467d2aad4c07e2b6a95d9766472b7f83f4f4c
Robert Percival <robert@mactechs.com>
2016-03-21 14:09:51 -0700

    Initial commit

---
+++ DockEditor.py
@@ -0,0 +1,71 @@
+import os
+import uuid
+import CoreFoundation
+from Foundation import NSMutableArray
+
+
+class Dock(object):
+
+	def __init__(self):
+		self.app_dirs = ('/Applications/', 
+			'/Applications/Microsoft Office 2011/', 
+			'/Applications/Utilities/', 
+			'/System/Library/CoreServices/', 
+			'/System/Library/CoreServices/Applications/'
+		)
+		self.id          = "com.apple.dock"
+		self.apps        = NSMutableArray.alloc().initWithArray_(CoreFoundation.CFPreferencesCopyAppValue("persistent-apps", self.id))
+		self.others      = NSMutableArray.alloc().initWithArray_(CoreFoundation.CFPreferencesCopyAppValue("persistent-others", self.id))
+		self.labels      = None
+		self.identifiers = None
+
+	def add(self, add_name, index=-1):
+		try_paths = [path + add_name + '.app' for path in self.app_dirs]
+		add_path = [path for path in try_paths if os.path.exists(path)]
+
+		if not add_path:
+			print "Can't find app: %s" % (add_name)
+			return False
+		else:
+			add_path = add_path[0]
+
+		# if item already in dock do not add
+		for dock_item in self.apps:
+			if dock_item['tile-data'].get('file-label') == add_name:
+				print "item %s already found" % (add_name)
+				return False
+
+		label_name = add_name.split(".app")[0]
+
+		new_item = {
+			'GUID': str(uuid.uuid4()).upper(), 
+			'tile-data': {
+				'file-data': {
+					'_CFURLString': add_path, 
+					'_CFURLStringType': 0
+				},
+				'file-label': label_name, 
+				'file-type': 32
+			}, 
+			'tile-type': 'file-tile'
+		}
+		self.apps.append(new_item)
+		return True
+
+	def remove(self, item_name):
+		for dock_item in reversed(self.apps):
+			if dock_item['tile-data'].get('file-label') == item_name:
+				self.apps.remove(dock_item)
+				return
+
+	def move(self, item_name, index):
+		pass
+
+	def swap(self, item_name1, item_name2):
+		pass
+
+	def write(self):
+		CoreFoundation.CFPreferencesSetAppValue("persistent-apps", self.apps,  self.id)
+		CoreFoundation.CFPreferencesSetAppValue("persistent-others", self.others,  self.id)
+		CoreFoundation.CFPreferencesAppSynchronize(self.id)
+
